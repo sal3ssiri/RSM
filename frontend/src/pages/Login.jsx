c224d9773149fa652f74a44a28e6d68d2416e5b8
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 11:42:03 +0300

    Add Login component for property management system

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+
+  return (
+
+    <div>
+
+      <h1>
+        نظام إدارة العقارات
+      </h1>
+
+      <div id="googleSignIn"></div>
+
+    </div>
+
+  );
+
+}
