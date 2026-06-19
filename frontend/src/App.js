83a846ba2560c06ba5a657e8d14b621d75c81036
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 10:58:40 +0300

    Add App component with Dashboard and MainLayout

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Dashboard
+  from "./pages/Dashboard";
+
+import MainLayout
+  from "./layouts/MainLayout";
+
+export default function App() {
+
+  return (
+
+    <MainLayout>
+
+      <Dashboard />
+
+    </MainLayout>
+
+  );
+
+}
