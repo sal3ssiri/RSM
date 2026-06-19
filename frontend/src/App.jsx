7024e486fef6fcc820e54e3d8702079a9d92f79b
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 12:19:54 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
-import Login
-from "./pages/Login.jsx";
-
-import Dashboard
-from "./pages/Dashboard.jsx";
-
-import MainLayout
-from "./layouts/MainLayout.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
 export default function App() {
 
@@ -15,23 +10,10 @@ localStorage.getItem(
 );
 
 if (!user) {
-
-```
 return <Login />;
-```
-
 }
 
-return (
-
-```
-<MainLayout>
-
-  <Dashboard />
-
-</MainLayout>
-```
-
+return ( <MainLayout> <Dashboard /> </MainLayout>
 );
 
 }
