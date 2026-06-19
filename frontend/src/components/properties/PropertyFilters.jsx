25aa7a32fa8d22eeb6b0d6738da7e9d40ff67c9b
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 21:55:40 +0300

    Update PropertyFilters.jsx

--- frontend/src/components/properties/PropertyFilters.jsx
+++ frontend/src/components/properties/PropertyFilters.jsx
@@ -1,1 +1,146 @@
+export default function PropertyFilters(
+  {
+    search,
+    setSearch,
 
+    propertyType,
+    setPropertyType,
+
+    city,
+    setCity,
+
+    status,
+    setStatus,
+
+    propertyTypes,
+    cities,
+    statuses
+  }
+) {
+
+  return (
+
+    <div className="filters">
+
+      <input
+        type="text"
+        placeholder="ابحث باسم العقار أو الرقم أو الحي..."
+        value={search}
+        onChange={
+          e =>
+            setSearch(
+              e.target.value
+            )
+        }
+      />
+
+      <select
+        value={propertyType}
+        onChange={
+          e =>
+            setPropertyType(
+              e.target.value
+            )
+        }
+      >
+
+        <option value="">
+          كل الأنواع
+        </option>
+
+        {
+
+          propertyTypes.map(
+            item => (
+
+              <option
+                key={item}
+                value={item}
+              >
+
+                {item}
+
+              </option>
+
+            )
+          )
+
+        }
+
+      </select>
+
+      <select
+        value={city}
+        onChange={
+          e =>
+            setCity(
+              e.target.value
+            )
+        }
+      >
+
+        <option value="">
+          كل المدن
+        </option>
+
+        {
+
+          cities.map(
+            item => (
+
+              <option
+                key={item}
+                value={item}
+              >
+
+                {item}
+
+              </option>
+
+            )
+          )
+
+        }
+
+      </select>
+
+      <select
+        value={status}
+        onChange={
+          e =>
+            setStatus(
+              e.target.value
+            )
+        }
+      >
+
+        <option value="">
+          كل الحالات
+        </option>
+
+        {
+
+          statuses.map(
+            item => (
+
+              <option
+                key={item}
+                value={item}
+              >
+
+                {item}
+
+              </option>
+
+            )
+          )
+
+        }
+
+      </select>
+
+    </div>
+
+  );
+
+}
