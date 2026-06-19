53de42022d9c570c3c11adb859c94b8172c8be61
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 10:52:57 +0300

    Add configuration file with app settings

---
+++ frontend/src/config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+
+  APP_NAME:
+    "RSM",
+
+  APP_VERSION:
+    "1.0.0",
+
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbw0dKIj7sUmNUBkWEwjCN61pyDcqrMmmLIfZhvIk3qIkiHDCrW2PNWUEN_2N9TIX-I6Yg/exec",
+
+  SESSION_KEY:
+    "rsm_token"
+
+};
