605abae0e7863f3f8b78d501165a403b1d88dd81
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 12:38:14 +0300

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,97 @@
+import {
+LayoutDashboard,
+Building2,
+Users,
+FileText,
+Wallet,
+UserCog,
+LogOut
+}
+from "lucide-react";
+
+import {
+NavLink
+}
+from "react-router-dom";
+
 export default function Sidebar() {
 
-  return (
+return (
 
-    <div>
 
-      Sidebar
+<aside className="sidebar">
 
-    </div>
+  <div className="sidebar-logo">
 
-  );
+    RSM
+
+  </div>
+
+  <nav>
+
+    <NavLink
+      to="/"
+    >
+      <LayoutDashboard size={18} />
+      Dashboard
+    </NavLink>
+
+    <NavLink
+      to="/properties"
+    >
+      <Building2 size={18} />
+      Properties
+    </NavLink>
+
+    <NavLink
+      to="/tenants"
+    >
+      <Users size={18} />
+      Tenants
+    </NavLink>
+
+    <NavLink
+      to="/contracts"
+    >
+      <FileText size={18} />
+      Contracts
+    </NavLink>
+
+    <NavLink
+      to="/payments"
+    >
+      <Wallet size={18} />
+      Payments
+    </NavLink>
+
+    <NavLink
+      to="/users"
+    >
+      <UserCog size={18} />
+      Users
+    </NavLink>
+
+  </nav>
+
+  <button
+    onClick={() => {
+
+      localStorage.clear();
+
+      window.location.reload();
+
+    }}
+  >
+
+    <LogOut size={18} />
+
+    Logout
+
+  </button>
+
+</aside>
+
+
+);
 
 }
