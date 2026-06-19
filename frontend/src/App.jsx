917f12e67479a04a43a7819f8b7b03acb5295e39
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 11:46:53 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
-import Dashboard
-  from "./pages/Dashboard.jsx";
-
-import MainLayout
-  from "./layouts/MainLayout.jsx";
+import Login
+  from "./pages/Login.jsx";
 
 export default function App() {
 
-  return (
-
-    <Login />
-
-  );
+  return <Login />;
 
 }
