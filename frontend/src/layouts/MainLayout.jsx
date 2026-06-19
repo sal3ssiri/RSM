06176b51cb692aa19a35a1303ff7a27f46b9d2e6
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 11:29:18 +0300

    Update import paths to include file extensions

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,8 +1,8 @@
 import Header
-  from "../components/Header";
+  from "../components/Header.jsx";
 
 import Sidebar
-  from "../components/Sidebar";
+  from "../components/Sidebar.jsx";
 
 export default function MainLayout(
   { children }
