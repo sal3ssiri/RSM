e921b9d0895488b4780afcc0c99b0950d299b85e
sal3ssiri <s.al3ssiri@gmail.com>
2026-06-19 12:38:59 +0300

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,64 @@
+import {
+Bell,
+UserCircle
+}
+from "lucide-react";
+
 export default function Header() {
 
-  return (
+const user =
+JSON.parse(
+localStorage.getItem(
+"user"
+)
+);
 
-    <div>
+return (
 
-      Header
+
+<header className="header">
+
+  <div className="header-title">
+
+    Real Estate Management System
+
+  </div>
+
+  <div className="header-actions">
+
+    <button
+      className="icon-button"
+    >
+
+      <Bell size={20} />
+
+    </button>
+
+    <div className="user-info">
+
+      <UserCircle
+        size={32}
+      />
+
+      <div>
+
+        <div>
+          {user?.name}
+        </div>
+
+        <small>
+          {user?.role}
+        </small>
+
+      </div>
 
     </div>
 
-  );
+  </div>
+
+</header>
+
+
+);
 
 }
